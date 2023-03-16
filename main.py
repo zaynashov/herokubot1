544612fc3b9d7f45af09f9c5b9004dafb98f723d
zaynashov <106890018+zaynashov@users.noreply.github.com>
2023-03-17 00:40:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import json
+import asyncio
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 from flask import Flask, request
+
+BOT_TOKEN = '6068250934:AAHB33mkYKBRbwXcArCO7tOXospey3eJOLA'
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+def send_message():
+    # замените CHAT_ID на ID чата, в который вы хотите отправить сообщение
+    chat_id = '5318107862'
+
+    # замените TEXT на текст вашего сообщения
+    text = (ticker)
+
+    # создаем объект сообщения
+    message = types.Message(text=text)
+
+    # отправляем сообщение
+    bot.send_message(chat_id=chat_id, text=message.text)
 
 app = Flask(__name__)
 
@@ -11,11 +33,12 @@ def webhook():
     ticker = data['ticker']
     price = data['age']
     signal = data['signal']
-
-    # выполним какие-то действия с этими данными
-    print(f"Получены данные: имя - {ticker}, возраст - {price}, email - {signal}")
+    send_message()
 
     return 'OK'
 
 if __name__ == '__main__':
     app.run()
+
+
+
