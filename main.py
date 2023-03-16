9e8986dc8750f6fd4757436b132b237c084e36ea
zaynashov <106890018+zaynashov@users.noreply.github.com>
2023-03-16 23:03:01 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# import main Flask class and request object
+from flask import Flask, request
+
+# create the Flask app
+app = Flask(__name__)
+
+@app.route('/query-example')
+def query_example():
+    # if key doesn't exist, returns None
+    language = request.args.get('language')
+    print(language)
+
+    return '''<h1>Webhook accepted!</h1>''' #.format(language)
+
+@app.route('/form-example')
+def form_example():
+    return 'Form Data Example'
+
+@app.route('/json-example')
+def json_example():
+    return 'JSON Object Example'
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=False, port=443)
