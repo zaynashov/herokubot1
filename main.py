6e340dca9e61f5d1b9d1185d3da54784f278f124
zaynashov <106890018+zaynashov@users.noreply.github.com>
2023-03-16 23:44:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-# import main Flask class and request object
+import json
 from flask import Flask, request
 
-# create the Flask app
 app = Flask(__name__)
 
-@app.route('/query-example')
-def query_example():
-    # if key doesn't exist, returns None
-    language = request.args.get('language')
-    print(language)
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.get_json()
 
-    return '''<h1>Webhook accepted!</h1>''' #.format(language)
+    # распарсиваем данные и прикрепляем к переменным
+    ticker = data['ticker']
+    price = data['age']
+    signal = data['signal']
 
-@app.route('/form-example')
-def form_example():
-    return 'Form Data Example'
+    # выполним какие-то действия с этими данными
+    print(f"Получены данные: имя - {ticker}, возраст - {price}, email - {signal}")
 
-@app.route('/json-example')
-def json_example():
-    return 'JSON Object Example'
+    return 'OK'
 
 if __name__ == '__main__':
-    # run app in debug mode on port 5000
-    app.run(debug=False, HOST='0.0.0.0', port='80')
+    app.run()
